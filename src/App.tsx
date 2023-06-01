662a072b7d6a3c68a49d84a8f56ab50271f8f67c
Ikbal <hikmat.iqbal@gmail.com>
2023-06-01 22:04:18 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AppContext } from './context';
 import { AppContextType } from './types/app';
 import SearchResult from './components/SearchResult';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App(): JSX.Element {
   const [focus, setFocus] = useState<boolean>(false);
@@ -13,18 +14,23 @@ function App(): JSX.Element {
 
   return (
     <>
-      <Header />
-      <div
-        onMouseDown={() => setFocus(true)}
-        onMouseLeave={() => setFocus(false)}
-        className={classNames(
-          'w-11/12 md:w-full max-w-2xl mx-auto items-center justify-center bg-white rounded-xl transition-all shadow-indigo-200 my-10',
-          focus || users.length > 0 ? 'shadow-xl' : 'shadow'
-        )}
-      >
-        <SearchInput />
-        <SearchResult />
-      </div>
+      <main className="min-h-[60vh] mb-32">
+        <Header />
+        <div
+          onMouseDown={() => setFocus(true)}
+          onMouseLeave={() => setFocus(false)}
+          className={classNames(
+            'w-11/12 md:w-full mx-auto items-center justify-center bg-white rounded-xl transition-all shadow-indigo-200 my-10',
+            focus || users.length > 0
+              ? 'shadow-xl max-w-2xl'
+              : 'shadow max-w-lg'
+          )}
+        >
+          <SearchInput />
+          <SearchResult />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="p-5 text-center text-slate-500 flex flex-col">
+      <a
+        className="text-sm hover:text-indigo-600 font-medium intro-y"
+        href="https://github.com/iikmoh"
+        target="_blank"
+        rel="noreferrer"
+      >
+        @iikmoh
+      </a>
+      <a
+        className="text-sm mt-1 hover:text-indigo-600 intro-y"
+        href={process.env.PUBLIC_URL}
+      >
+        Source available on{' '}
+        <span className="text-indigo-600 font-medium">Github</span>
+      </a>
+    </div>
+  );
+}
